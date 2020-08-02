bfc67edecdf440e99b2f795c72ef6855bd7649f6
asutegenov <asutegenov@gmail.com>
2020-08-03 00:18:39 +0400

    Revert "Добавление PageObject."
    This reverts commit 4af384f3721fb684644ef4ba3070aa6bae9f0af6.

--- pages/locators.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver import Remote as RemoteWebDriver
-from selenium.webdriver.common.keys import Keys 
-
-class YandexPageLocators():
-    SEARCH_INPUT = (By.ID, "text")
-    SEARCH_BUTTON = (By.CSS_SELECTOR, ".search2__button>.button")
--- tests.py
+++ tests.py
@@ -1,16 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from pages.main_page import MainPage
-from pages.locators import YandexPageLocators
-from pages.search import Search
+import time
 
-class TestYandex():
-  def test_search_yandex(self, browser):
+class TestSearsh():
+  def test_search(self):
     try:
       link = "https://yandex.ru"
-      pages = Search(browser, link)
-      pages.open()
-      pages.check_equal_url_and_input_date()
+      browser = webdriver.Chrome()
+      browser.get(link)
+
+      search_input = browser.find_element(By.ID, 'text')
+      search_input.send_keys("leinster")
+      search_button = browser.find_element(By.CSS_SELECTOR, '.search2__button>.button')
+      search_button.click()
+      
+      print(browser.current_url)
+      assert "leinster" in browser.current_url, "ERRORRRRRRRR!"
     finally:
       browser.quit()
-      
