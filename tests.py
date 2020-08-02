9bbd7c7227050e2c43e0da108e4c0740ef0bd17d
asutegenov <asutegenov@gmail.com>
2020-08-03 00:18:36 +0400

    Revert "Добавление проверки входа в Яндекс.Почта"
    This reverts commit c00ce0e94eb87526b56ebc718fe3efc9cb2cfe65.

--- tests.py
+++ tests.py
@@ -2,25 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
 from pages.locators import YandexPageLocators
-from pages.yandexPage import YandexPage
+from pages.search import Search
 
 class TestYandex():
-  # def test_search_yandex(self, browser):
-  #   try:
-  #     link = "https://yandex.ru"
-  #     pages = YandexPage(browser, link)
-  #     pages.open()
-  #     pages.check_equal_url_and_input_date()
-  #   finally:
-  #     browser.quit()
-  
-  def test_successful_login_in_yandex_mail(self, browser):
+  def test_search_yandex(self, browser):
     try:
-      test_login = input("Введите, пожалуйста, логин:  ")
-      test_passwd = input("Введите, пожалуйста, пароль: ")
       link = "https://yandex.ru"
-      pages = YandexPage(browser, link)
+      pages = Search(browser, link)
       pages.open()
-      pages.successful_login_in_yandex_mail(test_login, test_passwd)
+      pages.check_equal_url_and_input_date()
     finally:
       browser.quit()
+      
